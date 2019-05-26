daac22d981d17dcd81426f23b9bf62dcfa75f8ed
Abdullah Cangul <abdullahcangul@gmail.com>
2019-05-26 10:14:05 +0300

    Initial commit

---
+++ src/main/java/com/abdullahcangul/universite/UniversiteApplication.java
@@ -0,0 +1,13 @@
+package com.abdullahcangul.universite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniversiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniversiteApplication.class, args);
+	}
+
+}
